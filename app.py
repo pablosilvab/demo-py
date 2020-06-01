77e74a4f1b5407b2967dbcab2f0f9895ff747a99
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-05-31 23:27:54 -0400

    file read and tvshows endpoint

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, jsonify
+import os
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
-def summary():
+def home():
     return jsonify({"message": "Hello World"})
+
+@app.route('/tvshows')
+def tvshows():
+    filename = os.path.join(app.static_folder, 'tvshows.json')
+    with open(filename) as tvshows:
+        data = json.load(tvshows)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

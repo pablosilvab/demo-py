a4f6b0eeb13bc78c688232ca9dc6b19b0190d573
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-05-31 23:20:29 -0400

    refactor code

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-PACKAGE_NAME=hello-world
+PACKAGE_NAME=demo-python-docker
 VERSION=1.0.0.SNAPSHOT
 PORT = 5000
 DOCKER_HUB=pablon27 
@@ -7,4 +7,10 @@ docker-build:
 	docker build -t ${PACKAGE_NAME}:${VERSION} .
 
 docker-shell:
-	docker run -p ${PORT}:5000 ${PACKAGE_NAME}:${VERSION}
+	docker run --rm -p ${PORT}:5000 ${PACKAGE_NAME}:${VERSION}
+
+run:
+	python3 app.py
+
+install:
+	pip3 install -r requirements.txt
--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-# app.py - a minimal flask api using flask_restful
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-api.add_resource(HelloWorld, '/')
+@app.route('/')
+def summary():
+    return jsonify({"message": "Hello World"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
